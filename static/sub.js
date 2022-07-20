8dd5e2f33f79b914fbd60205444a99fdad362920
laur <miss.lauren.pak@gmail.com>
2022-07-20 10:40:52 +0800

    dumbo
    hello everyoen

---
+++ static/sub.js
@@ -0,0 +1,18 @@
+
+
+// const readMoreBtn= document.querySelector('.read-more-btn');
+// const text = document.querySelector('.text');
+// document.querySelector('.read-more-btn').addEventListener('click', (e)=>{
+//     alert('hi')
+//     text.classList.toggle('show-more')
+//     if(readMoreBtn.innerText === "Read More"){
+//         readMoreBtn.innerText = 'Read Less';
+
+//     }else{
+//         readMoreBtn.innerText = 'Read More';
+//     }
+// })
+
+
+
+
