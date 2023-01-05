ff25b724e0fa4c0eba5eac0c3b5d300c1971e660
laur <miss.lauren.pak@gmail.com>
2023-01-05 16:44:38 +0800

    migration stuff after breakdown
    lAPTOP ONLY

---
+++ migrations/versions/6b637c8ce0a2.py
@@ -0,0 +1,30 @@
+from alembic import op
+import sqlalchemy as sa
+
+revision = '6b637c8ce0a2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    op.create_table('users',
+    sa.Column('id', sa.Integer(), primary_key=True),
+    sa.Column('username', sa.String(Length=50), nullable=False),
+    sa.Column('date_added', sa.DateTime),
+    sa.Column('email', sa.String(50), unique=True, nullable = False),
+    sa.Column('password',sa.String(50), nullable = False)
+    )
+
+    op.create_table('posts',
+    sa.Column('id', sa.Integer(), primary_key=True),
+    sa.Column('title', sa.String(Length=255)),
+    sa.Column('content', sa.Text(1000)),
+    sa.Column('author', sa.String(255)),
+    sa.Column('slug', sa.String(255))
+    )
+
+
+
+def downgrade():
+    op.drop_table('posts')
+    op.drop_table('users')
--- website/__init__.py
+++ website/__init__.py
@@ -4,9 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from os import path
+from sqlalchemy import MetaData
+convention={
+     "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata= MetaData(naming_convention=convention)
 app = Flask(__name__)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db, render_as_batch=True)
 
 def createapp():
 
